0f723bb69f346c63142ee49dd1a25aa8eda7f411
lymos <lymos@lym.com>
2022-07-06 10:56:04 +0800

    stripe charge

--- src/stripe.php
+++ src/stripe.php
@@ -3,14 +3,22 @@ namespace Lymos\Stripe;
 
 use Lymos\Stripe\api\charge;
 
+
 class stripe{
 
-    public function key(){
+    public $key = '';
 
+    public function key($key){
+        $this->key = $key;
+        return $this;
     }
 
-    public function charge(){
+    public function charge($data, $options = []){
         $charge = new charge;
-        $charge->chargeSend();
+        if($this->key){
+            $options['userpwd'] = $this->key;
+        }
+        $options['is_http_build'] = true;
+        return $charge->chargeSend($this->key, $data, $options);
     }
 }
